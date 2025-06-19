cefaaadd11b66ec6b01babb624369cd278b196a6
JaydenKJ <160088283+JaydenKJ@users.noreply.github.com>
2025-06-19 14:34:07 -0700

    Create sw.js

---
+++ library/sw.js
@@ -0,0 +1,10 @@
+// sw.js
+self.addEventListener("install", () => {
+  self.skipWaiting();
+});
+self.addEventListener("activate", () => {
+  clients.claim();
+});
+self.addEventListener("fetch", event => {
+  event.respondWith(fetch(event.request));
+});
